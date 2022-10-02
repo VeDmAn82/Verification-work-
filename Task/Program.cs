22c6d45d1863ca912c55413c543fc683124642dd
Dmitry Vechkhayzer <dmitry.v82@mail.ru>
2022-10-02 15:36:22 +0400

    add decision Task

---
+++ Task/Program.cs
@@ -0,0 +1,32 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+// длина которых меньше или равна 3 символам.
+
+string[] array = {"hello", "2", "world", ":-)"}; 
+string[] resultArray = new string[array.Length];
+
+FillArray(array);
+Console.WriteLine();
+Print(resultArray);
+
+void FillArray (string[] array)
+{
+     int index = 0;
+     for (int i=0; i < array.Length; i++)
+     {      
+        if(array[i].Length <= 3)
+        {
+        resultArray[index] = array[i];
+        index++;
+        }
+     } 
+}
+
+void Print(string[] array)
+{
+    Console.Write("[");
+    for (int i=0; i<array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.Write("]");
+}
